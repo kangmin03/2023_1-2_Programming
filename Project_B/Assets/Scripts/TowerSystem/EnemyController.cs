4b3e7bcfff66178134aec52e6b3f743e714635b6
kangmin03 <lkm8276@g.shingu.ac.kr>
2023-11-03 09:50:14 +0900

    Feat "슬로우 타워까지 구현"
    슬로우 타워 구현

--- Project_B/Assets/Scripts/TowerSystem/EnemyController.cs
+++ Project_B/Assets/Scripts/TowerSystem/EnemyController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    public float speedMod = 1.0f;
+    public float timeSinceStart = 0.0f;
+    public bool modeEnd = true;
+
     public float moveSpeed;
 
     private EnemyPath thePath;
@@ -22,12 +26,23 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (modeEnd == false)
+        {
+            timeSinceStart -= Time.deltaTime;
+
+            if(timeSinceStart <= 0.0f)
+            {
+                speedMod = 1.0f;
+                modeEnd = true;
+            }
+        }
+
         if(reacheEnd == false)
         {
             transform.LookAt(thePath.points[currentPoint]);
 
             transform.position =
-                Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime);
+                Vector3.MoveTowards(transform.position, thePath.points[currentPoint].position, moveSpeed * Time.deltaTime * speedMod);
 
             if (Vector3.Distance(transform.position, thePath.points[currentPoint].position) < 0.01f)
             {
@@ -38,5 +53,14 @@ public class EnemyController : MonoBehaviour
                 }
             }
         }
+
+        
+    }
+
+    public void SetMode(float Value)
+    {
+        modeEnd = false;
+        speedMod = Value;
+        timeSinceStart = 2.0f;
     }
 }
---
+++ Project_B/Assets/Scripts/TowerSystem/EnemyHealthController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthController : MonoBehaviour
+{
+
+    public int totalHealth = 50;
+
+    public void TakeDamage(int damageAmount)
+    {
+        totalHealth -= damageAmount;
+
+        if (totalHealth <= 0)
+        {
+            totalHealth = 0;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+
+
+
+}
